09d48a4bc75a6db1c13bd070625ec3b389bd6ffc
claes <claeswdev@gmail.com>
2025-03-04 17:32:54 +0100

    Add styled-components dependency and created CardContainer

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,14 +1,30 @@
-import { WeatherData } from '../types';
+import styled from "styled-components";
+import { WeatherData } from "../types";
+
+const CardContainer = styled.div`
+  border: 1px solid black;
+  width: 300px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+  background-color: #f9f9f9;
+  color: #333;
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+`;
 
 const Card = ({ weatherData }: { weatherData: WeatherData }) => {
   return (
     <>
-      <p>Location: {weatherData.location.name}</p>
-      <p>Local time: {weatherData.location.localtime}</p>
-      <p>Condition: {weatherData.current.condition.text}</p>
-      <p>Temperature: {weatherData.current.temp_c}</p>
+      <CardContainer>
+        <p>Location: {weatherData.location.name}</p>
+        {/* <p>Local time: {weatherData.location.localtime}</p> */}
+        <p>Condition: {weatherData.current.condition.text}</p>
+        <p>Temperature: {weatherData.current.temp_c}&deg; C</p>
+      </CardContainer>
     </>
-  )
-}
+  );
+};
 
 export default Card;
